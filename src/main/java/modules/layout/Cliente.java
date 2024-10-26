5c348842e0cda70d897970c77ea76bf05cb300ad
mardoqueudickson1 <mardoqueudrums1999@gmail.com>
2024-10-26 23:47:46 +0100

    feat: many adjust

--- src/main/java/modules/layout/Cliente.java
+++ src/main/java/modules/layout/Cliente.java
@@ -1,15 +1,32 @@
 package modules.layout;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import modules.models.ClienteModel;
+import modules.models.ProdutoModel;
+import modules.cliente.repository.ClienteRepository;
 
 
 @Controller
 public class Cliente {
 
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+
     @GetMapping("/clientes")
-    public String index() {
+    public String index(Model model) {
+        List<ClienteModel> produtos = clienteRepository.findAll();
+        model.addAttribute("cliente", produtos);
+
+        ProdutoModel produtoModel = new ProdutoModel(); 
+        model.addAttribute("produtoModel", produtoModel);
         return "clientes";
     }
     
