723581dc22024a7d2c68149c218d215e4eb0397f
mardoqueudickson1 <mardoqueudrums1999@gmail.com>
2024-10-27 02:59:11 +0100

    feat: adding sales form

--- src/main/java/modules/layout/Cliente.java
+++ src/main/java/modules/layout/Cliente.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import modules.models.ClienteModel;
 import modules.models.ProdutoModel;
@@ -28,6 +30,16 @@ public class Cliente {
         ProdutoModel produtoModel = new ProdutoModel(); 
         model.addAttribute("produtoModel", produtoModel);
         return "clientes";
+    }
+
+
+
+    @PostMapping("/clientes/registrar")
+    public String adicionarNovoCliente(@ModelAttribute ClienteModel cliente) {
+        System.err.println("Dados" + cliente);
+        
+        clienteRepository.save(cliente);
+        return "redirect:/clientes";
     }
     
 }
